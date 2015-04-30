237510a2562e422c0da12b7060884f2e89050ddc
aonotas <nanigashi03@gmail.com>
2015-04-30 19:17:10 +0900

    config 追加

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # paragraph_vector
 Paragraph Vector code
+
+
+Media Wikiをローカルのmwikiに入っていることが必要です。
+
+
+python make_pv.py --corpus=wiki
---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+database_config = {
+        "host": "localhost",
+        "db": "mwiki",
+        "user": "root",
+        "passwd": ""
+        }
